6b8ae8ea5e1ee545460b88d442803b8e564914f0
Yoo JiSang <digndign2@naver.com>
2017-04-25 17:04:52 +0900

    Vertex Initialize 정점버퍼 초기화

--- DirectX3DPractice.cpp
+++ DirectX3DPractice.cpp
@@ -20,8 +20,20 @@ HRESULT InitD3D(HWND hWnd);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-LPDIRECT3D9			g_pD3D = NULL;
-LPDIRECT3DDEVICE9	g_pd3dDevice = NULL;
+LPDIRECT3D9				g_pD3D = NULL;
+LPDIRECT3DDEVICE9		g_pd3dDevice = NULL;
+LPDIRECT3DVERTEXBUFFER9 g_pVB = NULL;
+
+/// 사용자 정점을 정의할 구조체
+struct CUSTOMVERTEX
+{
+	FLOAT x, y, z, rhw;	/// 정점의 변환된 좌표(rhw 값이 있으면 변환이 완료된 정점이다.
+	DWORD color;		/// 정점의 색깔
+};
+
+/// 사용자 정점 구조체에 관한 정보를 나타내는 FVF 값
+/// 구조체는 X, Y, Z, RHW 값과 Diffuse 색깔값으로 이루어져 있음을 알 수 있다.
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
 
 HRESULT InitD3D(HWND hWnd)
 {
@@ -62,28 +74,72 @@ VOID Cleanup()
 	if (g_pD3D != NULL)
 		g_pD3D->Release();
 }
-/**-------------------------------------------------------------------
-*초기화된 객체들을 소거한다.
-*-------------------------------------------------------------------
-*/
+/**--------------------------------------------------------------------
+  * 정점 버퍼를 생성하고 정점 값을 채워 넣는다.
+  * 정점 버퍼란 기본적으로 정점 정보를 갖고 있는 메모리 블록이다.
+  * 정점 버퍼를 생성한 다음에는 반드시 Lock()과 Unlock()으로 포인터를 얻어내서
+  * 정점 정보를 정점 버퍼에 써넣어야 한다.
+  * 또한 D3D는 인덱스 버퍼도 사용 가능하다는 것을 명심하자.
+  * 정점 버퍼나 인덱스 버퍼는 기본 시스템 메모리 외에 디바이스 메모리(비디오카드 메모리)
+  * 에 생성될 수 있는데, 대부분의 비디오카드에서는 이렇게 할 경우 엄청난 속도의 향상을
+  * 얻을 수 있다.
+  *///-------------------------------------------------------------------
+HRESULT InitVB()
+{
+	///삼각형을 렌더링하기 위해 세 개의 정점을 선언
+	CUSTOMVERTEX vertices[] =
+	{
+		{150.0f, 50.0f, 0.5f, 1.0f, 0xffff0000, }, // x, y, z, rhw, color
+		{250.0f, 250.0f, 0.5f, 1.0f, 0xff00ff00,},
+		{50.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff,},
+	};
+
+	/// 정점 버퍼 생성
+	/// 3개의 사용자 정점을 보관할 메모리를 할당한다.
+	/// FVF를 지정하여 보관할 데이터의 형식을 지정한다.
+	if (FAILED(g_pd3dDevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+		0, D3DFVF_CUSTOMVERTEX,
+		D3DPOOL_DEFAULT, &g_pVB, NULL)))
+	{
+		return E_FAIL;
+	}
+
+	/// 정점 버퍼를 값으로 채운다.
+	/// 정점 버퍼의 Lock() 함수를 호출하여 포인터를 얻어온다.
+	VOID* pVertices;
+	if (FAILED(g_pVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0)))
+		return E_FAIL;
+	memcpy(pVertices, vertices, sizeof(vertices));
+	g_pVB->Unlock();
+
+	return S_OK;
+}
+/**--------------------------------------------------------------------
+* 화면 그리기
+*///-------------------------------------------------------------------
 VOID Render()
 {
-	if (NULL == g_pd3dDevice)
-		return;
-
 	/// 후면 버퍼를 파란색(0, 0, 255)으로 지운다.
-	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET,
+		D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 
 	/// 렌더링 시작
 	if (SUCCEEDED(g_pd3dDevice->BeginScene()))
 	{
-		/// 실제 렌더링 명령들이 나열될 곳
+		/// 정점 버퍼의 삼각형을 그린다.
+		/// 1. 정점 정보가 담겨있는 정점 버퍼를 출력 스트림으로 할당한다.
+		g_pd3dDevice->SetStreamSource(0, g_pVB, 0,
+			sizeof(CUSTOMVERTEX));
+		/// 2. D3D에 정점 셰이더 정보를 지정한다. 대부분의 경우에는 FVF만 지정한다.
+		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+		/// 3. 기하 정보를 출력하기 위한 DrawPrimitive () 함수 호출
+		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
 		/// 렌더링 종료
 		g_pd3dDevice->EndScene();
 	}
 
-	/// 후면 버퍼를 보이는 화면으로
+	/// 후면 버퍼를 보이는 화면으로!
 	g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 }
 /**-------------------------------------------------------------------
@@ -98,11 +154,6 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		Cleanup();
 		PostQuitMessage(0);
 		return 0;
-
-	case WM_PAINT:
-		Render();
-		ValidateRect(hWnd, NULL);
-		return 0;
 	}
 
 	return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -115,8 +166,8 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 {
 	/// 윈도우 클래스 등록
 	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,
-	GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
-	"D3D Tutorial",NULL };
+		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
+		"D3D Tutorial",NULL };
 	RegisterClassEx(&wc);
 
 	/// 윈도우 생성
@@ -127,16 +178,30 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 	/// Direct3D 초기화
 	if (SUCCEEDED(InitD3D(hWnd)))
 	{
-		/// 윈도우 출력
-		ShowWindow(hWnd, SW_SHOWDEFAULT);
-		UpdateWindow(hWnd);
-
-		/// 메시지 루프
-		MSG msg;
-		while (GetMessage(&msg, NULL, 0, 0))
+		/// 정점 버퍼 초기화
+		if (SUCCEEDED(InitVB()))
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			/// 윈도우 출력
+			ShowWindow(hWnd, SW_SHOWDEFAULT);
+			UpdateWindow(hWnd);
+
+			/// 메시지 루프
+			MSG msg;
+			ZeroMemory(&msg, SW_SHOWDEFAULT);
+			while (msg.message != WM_QUIT)
+			{
+				///메시지 큐에 메시지가 있으면 메시지 처리
+				if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+				{
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+				}
+				else
+				{
+					/// 처리할 메시지가 없으면 Render() 함수 호출
+					Render();
+				}
+			}
 		}
 	}
 
@@ -144,3 +209,4 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 	UnregisterClass("D3D Tutorial", wc.hInstance);
 	return 0;
 }
+
